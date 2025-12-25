e98b54869e8a7ebf11732babec2c8c36ca41b8f0
Emilio Simon <emsigo@Mac-Studio-de-Emilio.local>
2025-12-25 18:37:56 +0100

    fix: optimización de instancia prisma para vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['error'],
+  })
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: PrismaClientSingleton | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-})
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma

e71168ab6fcf4e8111429d398e07e0341337bd3a
Emilio Simon Gómez <emsigo@iMac-de-Emilio.local>
2025-12-26 18:42:54 +0100

    fix: añadir onNavigate al Header

--- app/App.tsx
+++ app/App.tsx
@@ -83,7 +83,7 @@ function AppContent() {
         />
 
         <div className="flex-1 flex flex-col min-w-0 h-screen overflow-hidden lg:ml-64 transition-all duration-300">
-          <Header usuario={usuario} onLogout={logout} />
+          <Header usuario={usuario} onLogout={logout} onNavigate={handleNavigate} />
           
           <main className="flex-1 overflow-y-auto p-4 lg:p-8 relative bg-slate-50">
             <div className="max-w-7xl mx-auto pb-20">
